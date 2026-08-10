8e6889d7de9b986e86039a02da8a025a4349335e
matepeira <mateopeirano21@gmail.com>
2026-08-10 10:24:56 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿const string kioscoName = "Kiosco Austral";
+﻿using System.Dynamic;
+
+const string kioscoName = "Kiosco Austral";
 
 Console.Write("Ingrese el nombre del cajero: ");
 String cajeroName = Console.ReadLine();
@@ -9,3 +11,10 @@ Console.WriteLine($"Bienvenido, {cajeroName}, caja abierta");
 
 Console.Write("Ingrese el nombre del producto: ");
 string productName = Console.ReadLine();
+
+Console.Write("Ingrese el precio del producto");
+decimal productPrice = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine($"Su producto es {productName} y su precio ${productPrice}");
+
+Console.ReadKey();
