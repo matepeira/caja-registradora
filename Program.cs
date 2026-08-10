44392879e3b0acb3c3d588ca20d4db9ab6f91257
matepeira <mateopeirano21@gmail.com>
2026-08-10 16:27:41 -0300

    etapa 3: carga multiple con menu

--- Program.cs
+++ Program.cs
@@ -9,12 +9,39 @@ Console.WriteLine($"-----{kioscoName}-----");
 Console.WriteLine($"Cajero a cargo: {cajeroName}");
 Console.WriteLine($"Bienvenido, {cajeroName}, caja abierta");
 
-Console.Write("Ingrese el nombre del producto: ");
-string productName = Console.ReadLine();
+int choice;
+decimal totalVenta = 0;
+int productCant = 0;
 
-Console.Write("Ingrese el precio del producto");
-decimal productPrice = decimal.Parse(Console.ReadLine());
+do
+{
+    Console.WriteLine("\nQue desea hacer?");
+    Console.WriteLine("1- Cargar un poducto");
+    Console.WriteLine("2- Cerrar la venta");
+    Console.WriteLine("Eleccion 1 o 2: ");
+    choice = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Su producto es {productName} y su precio ${productPrice}");
+    switch (choice)
+    {
+        case 1:
+            Console.Write("Ingrese el nombre del producto: ");
+            string productName = Console.ReadLine();
 
-Console.ReadKey();
+            Console.Write("Ingrese el precio del producto: ");
+            decimal productPrice = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Su producto es {productName} y su precio ${productPrice}");
+            totalVenta += productPrice;
+            productCant++;
+            break;
+
+        case 2:
+            Console.WriteLine("\nLa venta ha sido cerrada, cantidades y precio final: ");
+            Console.WriteLine($"Cantidad de productos: {productCant}");
+            Console.WriteLine($"Monto total: {totalVenta}");
+            break;
+        default:
+            Console.WriteLine("Opción inválida. Intente nuevamente.");
+            break;
+    }
+} while (choice != 2);
