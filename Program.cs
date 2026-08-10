fba334c5c96a0f338d2c481683f2824185e68c2b
matepeira <mateopeirano21@gmail.com>
2026-08-10 10:15:41 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿const string kioscoName = "Kiosco Austral";
+
+Console.Write("Ingrese el nombre del cajero: ");
+String cajeroName = Console.ReadLine();
+
+Console.WriteLine($"-----{kioscoName}-----");
+Console.WriteLine($"Cajero a cargo: {cajeroName}");
+Console.WriteLine($"Bienvenido, {cajeroName}, caja abierta");
+
+Console.Write("Ingrese el nombre del producto: ");
+string productName = Console.ReadLine();
